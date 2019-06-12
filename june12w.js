ea69ad94437e2316da5f660b69e013a6ac7924cf
Bhakti Bhakti <748535@student.tor.loc>
2019-06-12 16:24:33 -0400

    4:24 commit

---
+++ june12w.js
@@ -0,0 +1,11 @@
+function Strawberry() {
+
+    const calories = 180;
+    const calories2 = '180';
+
+    console.log(calories + 1);
+    console.log(calories2 + 1);
+
+}
+const showForm = false;
+console.log(showForm);
