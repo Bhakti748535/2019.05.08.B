835a325b2e7bea601c194540d2a65586d5e3d3e4
Bhakti Bhakti <748535@student.tor.loc>
2019-06-19 13:53:31 -0400

    june 19

---
+++ 2019.06.12.D/js1.js
@@ -0,0 +1,16 @@
+const Rabbit = {
+    typeofanimal: "Rabbit",
+    name: 'Bunny',
+    runningspeed: 60,
+    howfartillnaptime : 2,
+
+};
+
+const Turtle = {
+    typeofanimal: "Turtle",
+    name: 'Tonny',
+    runningspeed: 2,
+    howfartillnaptime : 0,
+
+};
+
