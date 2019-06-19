327592dc4395b36f3958e47eafed4213dbd17374
Bhakti Bhakti <748535@student.tor.loc>
2019-06-19 14:53:14 -0400

    2.53 june19

--- june19.js
+++ june19.js
@@ -8,3 +8,14 @@ const circle = {
         console.log('draw');
     }
 };
+//factory function
+function createCircle(radius) {
+    return {
+        radius,
+        draw: function() {
+            console.log('draw');
+        }
+    };
+}
+const circle = createCircle(1);
+circle.draw();
